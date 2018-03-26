f053703b4e4e4c95aaa2b4c109234e5c7f3d4c60
ufoscout <ufoscout@gmail.com>
2018-03-26 18:49:30 +0200

    update

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
+LINE_SEPARATOR='--------------------------------------------------------'
+
 ## Backend
 cd backend
 
+echo $LINE_SEPARATOR
+echo 'Build SpringBoot2-Java'
+echo $LINE_SEPARATOR
 cd SpringBoot2-Java
 mvn clean verify
 cd ..
 
+echo $LINE_SEPARATOR
+echo 'Build Vertx3-Kotlin'
+echo $LINE_SEPARATOR
 cd Vertx3-Kotlin
 mvn clean verify
 cd ..
@@ -14,7 +22,13 @@ cd ..
 ## Frontend
 cd frontend
 
+echo $LINE_SEPARATOR
+echo 'Build Angular-Typescript'
+echo $LINE_SEPARATOR
 cd Angular-Typescript
+rm -rf dist
+rm -rf node_modules
+npm install
 npm run test-single-run
 npm run build
 cd ..
