38efe31a869402f77402d49e9613937b33013650
ufoscout <ufoscout@gmail.com>
2018-04-23 20:57:37 +0200

    refactor kotiln

--- backend/Vertx3-Kotlin/src/main/java/com/weweb/auth/web/AuthenticationController.kt
+++ backend/Vertx3-Kotlin/src/main/java/com/weweb/auth/web/AuthenticationController.kt
@@ -40,6 +40,20 @@ class AuthenticationController (val router: Router,
             LoginResponseDto(token)
         }
 
+        router.get(AuthContants.BASE_AUTH_API + "/test/public").handler {
+            val authContext = auth.get(it.request())
+            it.request().response().endWithJson(authContext.user)
+        }
+
+        router.get(AuthContants.BASE_AUTH_API + "/test/authenticated").handler {
+            val authContext = auth.get(it).isAuthenticated()
+            it.request().response().endWithJson(authContext.user)
+        }
+
+        router.get(AuthContants.BASE_AUTH_API + "/test/protected").handler {
+            val authContext = auth.get(it).hasRole("ADMIN")
+            it.request().response().endWithJson(authContext.user)
+        }
 
     }
 
---
+++ backend/Vertx3-Kotlin/src/test/java/com/weweb/auth/AuthTestModule.kt
@@ -0,0 +1,11 @@
+package com.weweb.auth
+
+import org.kodein.di.Kodein
+
+
+object AuthTestModule {
+
+    fun module() = Kodein.Module(allowSilentOverride = true) {
+    }
+
+}
