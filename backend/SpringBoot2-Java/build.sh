a185edb7e18e827921308be8ae1656e0b71a3751
ufoscout <ufoscout@gmail.com>
2018-05-18 21:18:29 +0200

    Use docker slim images

--- backend/SpringBoot2-Java/build.sh
+++ backend/SpringBoot2-Java/build.sh
@@ -4,11 +4,8 @@ echo $LINE_SEPARATOR
 echo 'Build SpringBoot2-Java'
 echo $LINE_SEPARATOR
 
-mvn clean verify
+mvn -T 1C clean verify
 rc=$?
 if [[ $rc -ne 0 ]] ; then
   echo 'Failure'; exit $rc
 fi
-
-
-
--- backend/Vertx3-Kotlin/build.sh
+++ backend/Vertx3-Kotlin/build.sh
@@ -4,10 +4,8 @@ echo $LINE_SEPARATOR
 echo 'Build Vertx3-Kotlin'
 echo $LINE_SEPARATOR
 
-mvn clean verify
+mvn -T 1C clean verify
 rc=$?
 if [[ $rc -ne 0 ]] ; then
   echo 'Failure'; exit $rc
 fi
-
-
