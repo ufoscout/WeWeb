ad0344d690a536ee8a65cb2af4e4393f314a9dc8
Francesco Cina <ufoscout@gmail.com>
2018-07-07 17:44:27 +0200

    Add more nginx configuration vars

--- frontend/Angular-Typescript/docker_nginx/run.sh
+++ frontend/Angular-Typescript/docker_nginx/run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-envsubst '\$BACKEND_URL' <${ASSETS}/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '\$BACKEND_URL \$NGINX_WORKER_PROCESSES \$NGINX_WORKER_CONNECTIONS \$NGINX_LOGGER_LEVEL' <${ASSETS}/nginx.conf.template > /etc/nginx/nginx.conf
 
+echo "Nginx configuration:    "
+echo "------------------------"
 cat /etc/nginx/nginx.conf
+echo "------------------------"
+
 exec nginx -c /etc/nginx/nginx.conf
