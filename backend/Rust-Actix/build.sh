05672fcdf6e9fbee152b296b6fb7daf8577903d9
Francesco Cina <ufoscout@gmail.com>
2018-07-12 11:23:09 +0200

    [rust_actix] - Add jsonwebtoken dep and docker cmd to build static binary

--- backend/Rust-Actix/build.sh
+++ backend/Rust-Actix/build.sh
@@ -5,10 +5,19 @@ BUILD_TARGET_PATH=./target
 BUILD_TARGET_OS=x86_64-unknown-linux-musl
 BUILD_COPY_TO=./target/build
 
+BUILD_MUSL_STATIC_CMD="cargo build --release --target=${BUILD_TARGET_OS}"
+BUILD_MUSL_STATIC_CMD_WITH_DOCKER="
+      docker run --rm -it -v $(pwd):/home/rust/src \
+      -v $(pwd)/target/rust-musl-builder/cargo-git:/home/rust/.cargo/git \
+      -v $(pwd)/target/rust-musl-builder/cargo-registry:/home/rust/.cargo/registry \
+      ekidd/rust-musl-builder:latest \
+      /bin/bash -c \"sudo chown -R rust:rust /home/rust/.cargo/git /home/rust/.cargo/registry && cargo build --release\"
+      "
+
 declare -a steps=(
   "rm -rf ${BUILD_COPY_TO}"
   "cargo test"
-  "cargo build --release --target=${BUILD_TARGET_OS}"
+  "${BUILD_MUSL_STATIC_CMD_WITH_DOCKER}"
   "strip ${BUILD_TARGET_PATH}/${BUILD_TARGET_OS}/release/${PROJECT_NAME}"
   "mkdir ${BUILD_COPY_TO}"
   "cp -r ${BUILD_TARGET_PATH}/${BUILD_TARGET_OS}/release/${PROJECT_NAME} ${BUILD_COPY_TO}"
