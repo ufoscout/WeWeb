e54060c849623862483a03490aea05cc70356045
Francesco Cina <ufoscout@gmail.com>
2018-07-13 10:36:48 +0200

    [rust-actix] Add logger

--- backend/Rust-Actix/src/core/config/mod.rs
+++ backend/Rust-Actix/src/core/config/mod.rs
@@ -1,32 +1,49 @@
 extern crate config as rs_config;
 
 #[derive(Debug)]
-pub struct ServerConfig {
-    port: u32
+pub struct CoreConfig {
+    pub logger: LoggerConfig,
+    pub jwt: JwtConfig,
+    pub server: ServerConfig
+}
+
+#[derive(Debug)]
+pub struct LoggerConfig {
+    pub root_level: String,
+    pub level: String,
+    pub output_system_enable: bool,
+    pub output_file_enable: bool,
+    pub output_file_name: String
 }
 
 #[derive(Debug)]
 pub struct JwtConfig {
-    secret: String,
-    signature_algorithm: String,
-    token_validity_minutes: u32
+    pub secret: String,
+    pub signature_algorithm: String,
+    pub token_validity_minutes: u32
 }
 
 #[derive(Debug)]
-pub struct CoreConfig {
-    server: ServerConfig,
-    jwt: JwtConfig,
+pub struct ServerConfig {
+    pub port: u32
 }
 
 pub fn new(conf: rs_config::Config) -> CoreConfig {
     CoreConfig {
-        server: ServerConfig{
-            port: conf.get_int("server.port").unwrap() as u32
+        logger: LoggerConfig {
+            root_level: conf.get_str("logger.root_level").unwrap(),
+            level: conf.get_str("logger.level").unwrap(),
+            output_system_enable: conf.get_bool("logger.output_system_enable").unwrap(),
+            output_file_enable: conf.get_bool("logger.output_file_enable").unwrap(),
+            output_file_name: conf.get_str("logger.output_file_name").unwrap()
         },
         jwt: JwtConfig{
             secret: conf.get_str("jwt.secret").unwrap(),
             signature_algorithm: conf.get_str("jwt.signatureAlgorithm").unwrap(),
             token_validity_minutes: conf.get_int("jwt.tokenValidityMinutes").unwrap() as u32
+        },
+        server: ServerConfig{
+            port: conf.get_int("server.port").unwrap() as u32
         }
     }
 }
--- backend/Rust-Actix/src/lib.rs
+++ backend/Rust-Actix/src/lib.rs
@@ -1,10 +1,14 @@
 extern crate config;
 
-#[macro_use]
-extern crate serde_derive;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate log;
+extern crate fern;
+extern crate chrono;
 
 mod core;
 mod module;
+
+use std::str::FromStr;
 
 pub struct App {
     core: core::CoreModule
@@ -12,14 +16,24 @@ pub struct App {
 
 pub fn start() -> App {
 
+    println!("Startup - Start application setup... ");
+
     let mut settings = config::Config::default();
     settings
         .merge(config::File::with_name("./config/Config")).unwrap()
         .merge(config::Environment::new().separator("__")).unwrap();
 
 
+    println!("Startup - Load configuration... ");
+
     let conf = core::config::new(settings);
-    println!("{:?}", conf);
+
+    println!("Startup - Setup logger... ");
+
+    setup_logger(&conf.logger).unwrap();
+
+    println!("Starting application with configuration:\n{:#?}", conf);
+    info!("Starting application with configuration:\n{:#?}", conf);
 
     let app = App {
         core: core::new(conf)
@@ -28,6 +42,33 @@ pub fn start() -> App {
     module::start(&[&app.core]);
 
     return app;
+}
+
+fn setup_logger(logger_config: &core::config::LoggerConfig) -> Result<(), fern::InitError> {
+    let mut log_dispatcher = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::from_str(&logger_config.root_level).unwrap())
+        .level_for("rust_actix", log::LevelFilter::from_str(&logger_config.level).unwrap());
+
+    if logger_config.output_system_enable {
+        log_dispatcher = log_dispatcher.chain(std::io::stdout());
+    }
+
+    if logger_config.output_file_enable {
+        log_dispatcher = log_dispatcher.chain(fern::log_file(&logger_config.output_file_name)?);
+    }
+
+    log_dispatcher.apply()?;
+
+    Ok(())
 }
 
 #[cfg(test)]
