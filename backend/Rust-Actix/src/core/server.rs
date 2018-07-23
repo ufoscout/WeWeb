e864e6741a2f03d268678bcff8a5b14221dc5d99
Francesco Cina <ufoscout@gmail.com>
2018-07-23 15:17:14 +0200

    Add server module test

--- backend/Rust-Actix/src/core/server.rs
+++ backend/Rust-Actix/src/core/server.rs
@@ -46,3 +46,62 @@ impl Server {
         .run();
     }
 }
+
+#[cfg(test)]
+mod server_test {
+
+    extern crate actix_web;
+
+    use self::actix_web::{http, test};
+    use super::{Server, Router};
+    use core;
+
+    pub fn new_test_server(server: &Server) -> test::TestServer {
+        let clone = server.routers.clone();
+        test::TestServer::with_factory(move || {
+            let mut apps = vec![];
+            for router in clone.lock().unwrap().iter() {
+                apps.push(router.configure());
+            }
+            apps
+        })
+    }
+
+    #[test]
+    fn should_start_the_test_server_registering_all_modules() {
+        let config = core::config::ServerConfig { port: 0 };
+        let server = Server::new(&config);
+
+        server.register(Box::new(TestRouter{name: "one"}));
+        server.register(Box::new(TestRouter{name: "two"}));
+
+        let mut test_server = new_test_server(&server);
+
+        let request_one = test_server.client(http::Method::GET, "/one/hello").finish().unwrap();
+        let response_one = test_server.execute(request_one.send()).unwrap();
+        assert!(response_one.status().is_success());
+
+        let request_two = test_server.client(http::Method::GET, "/two/hello").finish().unwrap();
+        let response_two = test_server.execute(request_two.send()).unwrap();
+        assert!(response_two.status().is_success());
+
+        let request_three = test_server.client(http::Method::GET, "/three/hello").finish().unwrap();
+        let response_three = test_server.execute(request_three.send()).unwrap();
+        assert!(!response_three.status().is_success());
+    }
+
+    struct TestRouter{
+        name: &'static str
+    }
+
+    impl Router for TestRouter{
+        fn configure(&self) -> actix_web::App {
+            let name = self.name;
+            let mut path = String::from("/");
+            path.push_str(self.name);
+            let path = path.as_str();
+            actix_web::App::new().prefix(path).resource("/hello", move |r| r.f(move |_| format!("hello from {}", name)))
+        }
+    }
+
+}
