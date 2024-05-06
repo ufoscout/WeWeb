47ea632ff044f964c94d2a1cbf99a650bc7a2cfe
Francesco <ufoscout@gmail.com>
2024-05-06 21:25:03 +0200

    add tailwind

--- fullstack/leptos-tailwind/src/app.rs
+++ fullstack/leptos-tailwind/src/app.rs
@@ -8,6 +8,8 @@ pub fn App() -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
 
+    let (value, set_value) = create_signal(0);
+
     view! {
 
 
@@ -27,11 +29,26 @@ pub fn App() -> impl IntoView {
             }
             .into_view()
         }>
+  
+            <Title text="Leptos + Tailwindcss"/>
             <main>
+                <div class="bg-gradient-to-tl from-blue-800 to-blue-500 text-white font-mono flex flex-col min-h-screen">
+                    <div class="flex flex-row-reverse flex-wrap m-auto">
+                        <button on:click=move |_| set_value.update(|value| *value += 1) class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
+                            "+"
+                        </button>
+                        <button class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white">
+                            {value}
+                        </button>
+                        <button on:click=move |_| set_value.update(|value| *value -= 1) class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
+                            "-"
+                        </button>
+                    </div>
+                </div>
                 <Routes>
                     <Route path="" view=HomePage/>
                     <Route path="cargo" view=CargoTomlPage/>
-                    <Route path="todo" view=CargoTomlPage/>
+                    <Route path="todo" view=AddTodoPage/>
                 </Routes>
             </main>
         </Router>
